48ba6f0dcab251e5141c6968dc852cf13d43e597
ahmdkhled <ahmdkhled@gmail.com>
2018-07-12 13:41:02 +0200

    write detail activity tests

--- app/src/androidTest/java/com/ahmdkhled/popularmovies/DetailActivityTest.java
+++ app/src/androidTest/java/com/ahmdkhled/popularmovies/DetailActivityTest.java
@@ -2,13 +2,19 @@ package com.ahmdkhled.popularmovies;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.action.CloseKeyboardAction;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 
 import com.ahmdkhled.popularmovies.activities.DetailActivity;
 import com.ahmdkhled.popularmovies.activities.MainActivity;
 import com.ahmdkhled.popularmovies.model.Movie;
+import com.ahmdkhled.popularmovies.network.Urls;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,16 +22,23 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasData;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.IsNot.not;
 
 /**
  * Created by Ahmed Khaled on 7/12/2018.
  */
 
 @RunWith(JUnit4.class)
-public class DactivityTest {
+public class DetailActivityTest {
 
     @Rule
     public ActivityTestRule<DetailActivity> activityTestRule=
@@ -35,17 +48,62 @@ public class DactivityTest {
         protected Intent getActivityIntent() {
             Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
             Intent intent=new Intent(targetContext,DetailActivity.class);
-            intent.putExtra("movie",new Movie(363088,"test title","overview","fdf"
-                    ,"vcv",10));
+            intent.putExtra("movie",new Movie(299536,"Avengers: Infinity War",
+                    " this is a test overview",
+                    "fdf"
+                    ,"/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+                    8.4));
             return intent;
         }
             };
 
 
-    @Test
-    public void test(){
-        onView(withId(R.id.details_pager)).check(matches(isDisplayed()));
+    @Before
+    public void initIntents(){
+        Intents.init();
     }
 
+
+    @Test
+    public void testWidgetsDisplay(){
+        onView(withId(R.id.details_pager)).check(matches(isDisplayed()));
+        onView(withId(R.id.movie_cover)).check(matches(isDisplayed()));
+        onView(withId(R.id.vote)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testMovieDetails(){
+        onView(withId(R.id.movie_name)).check(matches(not(withText(""))));
+        onView(withId(R.id.overview)).check(matches(not(withText(""))));
+        onView(withId(R.id.release_date)).check(matches(not(withText(""))));
+    }
+
+    @Test
+    public void testTrailersClicking()  {
+        goToTraillersTab();
+
+        onView(withId(R.id.traillers_recycler)).perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+        intended(allOf(hasAction(Intent.ACTION_VIEW),hasData(Uri.parse(Urls.getYoutubeUrl("6ZfuNTqbHE8"))) ));
+    }
+
+    @After
+    public void releaseIntents(){
+        Intents.release();
+    }
+
+
+    public void goToTraillersTab() {
+        try {
+            activityTestRule.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    activityTestRule.getActivity().tab.getTabAt(1).select();
+
+                }
+            });
+        }catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
 
 }
--- app/src/main/java/com/ahmdkhled/popularmovies/activities/DetailActivity.java
+++ app/src/main/java/com/ahmdkhled/popularmovies/activities/DetailActivity.java
@@ -31,7 +31,7 @@ public class DetailActivity extends AppCompatActivity {
 
     ImageView poster;
     ViewPager pager;
-    TabLayout tab;
+    public TabLayout tab;
     TextView vote;
     boolean movieExist;
     public FloatingActionButton fab;
